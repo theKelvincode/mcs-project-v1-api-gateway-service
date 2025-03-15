4a30c7d97713abbe3c504f98bf656e01500511c4
theKelvinCode <thekelvincode@gmail.com>
2025-03-15 20:37:18 +0100

    Initial commit of all files

---
+++ src/test/java/com/oyas/mcs_project_v1/mcs_project_spring_cloud_gateway/McsProjectSpringCloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oyas.mcs_project_v1.mcs_project_spring_cloud_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McsProjectSpringCloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
